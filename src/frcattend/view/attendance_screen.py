a922163d066b47d58fbe495f81befa5e28a86ce6
Stacy Irwin <stacy.k.irwin@gmail.com>
2026-06-19 19:01:29 -0700

    Removed inactive toggle from student attendance screen.

--- src/frcattend/view/attendance_screen.py
+++ src/frcattend/view/attendance_screen.py
@@ -4,7 +4,6 @@ import textual
 from textual import app, binding, reactive, screen, widgets
 
 from frcattend import config, model, view
-from frcattend.view import inactive_toggle
 
 
 class StudentsTable(widgets.DataTable):
@@ -24,7 +23,7 @@ class StudentsTable(widgets.DataTable):
     def on_mount(self) -> None:
         """Initialize the table."""
         self.initialize_table()
-        self.update_table(include_inactive=False)
+        self.update_table()
 
     def initialize_table(self) -> None:
         """Set up table columns."""
@@ -35,22 +34,20 @@ class StudentsTable(widgets.DataTable):
             ("Graduation Year", "grad_year"),
             ("[green]Build Season Checkins[/]", "build_checkins"),
             ("All Checkins", "season_checkins"),
-            ("Last Checkin", "last_checkin"),
-            ("[yellow]Deactivated On[/]", "deactivated_on"),
+            ("Last Checkin", "last_checkin")
         ]:
             self.add_column(col[0], key=col[1])
 
-    def update_table(self, include_inactive: bool) -> None:
+    def update_table(self) -> None:
         """Populae the table with students."""
         self.clear(columns=False)
         self.students = {
             student.student_id: student
             for student in model.Attendance.get_student_attendance_students(
-                self.dbase, include_inactive=include_inactive
+                self.dbase, include_inactive=True
             )
         }
         for key, stu in self.students.items():
-            deactivated_on = "" if stu.deactivated_on is None else stu.deactivated_iso
             self.add_row(
                 f"[green]{stu.last_name}[/]",
                 stu.first_name,
@@ -58,7 +55,6 @@ class StudentsTable(widgets.DataTable):
                 f"[green]{stu.build_checkins}[/]",
                 stu.year_checkins,
                 stu.last_checkin,
-                f"[yellow]{deactivated_on}[/]",
                 key=key,
             )
         self.refresh()
@@ -141,7 +137,6 @@ class AttendanceScreen(screen.Screen):
     def compose(self) -> app.ComposeResult:
         """Add the datatable and other controls to the screen."""
         yield widgets.Header()
-        yield inactive_toggle.InactiveStudentToggle(id="attendance-inactive-toggle")
         yield StudentsTable(dbase=self.dbase, id="attendance-students-table")
         yield widgets.Static(
             "Events that Student Attended", classes="separator emphasis"
@@ -158,9 +153,3 @@ class AttendanceScreen(screen.Screen):
     ) -> None:
         """Set the new student_id, which will trigger a checkin table update."""
         self.student_id = message.row_key.value
-
-    @textual.on(widgets.Switch.Changed, "#attendance-inactive-toggle Switch")
-    def on_inactive_students_toggle(self, message: widgets.Switch.Changed) -> None:
-        """Toggle the inactive students on and off."""
-        table = self.query_one("#attendance-students-table", StudentsTable)
-        table.update_table(message.value)
