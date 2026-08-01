b2fe1c05b158842f6d7eec4e4689233f42fbebb4
Stacy Irwin <stacy.k.irwin@gmail.com>
2026-08-01 08:21:26 -0700

    Fixed circular import.
    All checks and tests pass.

--- src/frcattend/model/__init__.py
+++ src/frcattend/model/__init__.py
@@ -1,8 +1,17 @@
-"""The frcattend.model namespace."""
+"""The frcattend.model namespace.
 
-# ruff: noqa: F401
+For convenience, import commonly-used model classes into the frcattend.model
+namespace.
+
+The frcattend.model.students import must occurr first to prevent circular
+imports. Ignoring the I001 error prevents Ruff from resorting these imports.
+
+Suppressing the F401 rule allows unused variables.
+"""
+
+# ruff: file-ignore[F401, I001]
+from frcattend.model.students import Reason, Stage, Status, Student
 from frcattend.model.attendance import Attendance, AttendanceStudent
 from frcattend.model.database import DBase, DBaseError
 from frcattend.model.events_checkins import Checkin, Event, EventType, EventUpateError
-from frcattend.model.students import Reason, Stage, Status, Student
 from frcattend.model.surveys import Answer, Survey
